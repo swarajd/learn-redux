4e26f89dfdbefcbfeb94d48c25ed84664abe267c
swarage <swaraj.dhumne@gmail.com>
2015-12-28 22:14:58 -0500

    we are learning redux, this is a basic example

---
+++ js/main.js
@@ -0,0 +1,27 @@
+import { createStore } from 'redux';
+
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+let store = createStore(counter);
+
+const render = () => {
+  document.body.innerText = store.getState();
+}
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT'});
+});
+
+
