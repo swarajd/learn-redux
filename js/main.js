6f982d01c72bbcfaba35db05858d6213a851210d
swarage <swaraj.dhumne@gmail.com>
2015-12-28 22:16:11 -0500

    added documentation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux';
 
+//create a counter function
 const counter = (state = 0, action) => {
   switch(action.type) {
     case 'INCREMENT':
@@ -11,15 +12,20 @@ const counter = (state = 0, action) => {
   }
 }
 
+//create a store from it
 let store = createStore(counter);
 
+//function that renders the state of the store
 const render = () => {
   document.body.innerText = store.getState();
 }
 
+//whenver the state changes, run render
 store.subscribe(render);
 render();
 
+//whenever you click on the page, an action is 
+//dispatched to the store object and executed
 document.addEventListener('click', () => {
   store.dispatch({ type: 'INCREMENT'});
 });
